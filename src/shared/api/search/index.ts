4f6914b83722fad1b73d17bc6dc097d4553634af
이종혁 <whdgur57117@gmail.com>
2024-09-15 23:46:16 +0900

    기존 API 코드 삭제

--- src/shared/api/search/index.ts
+++
@@ -1,9 +0,0 @@
-import { getRequest } from 'shared/axios/instance';
-import type { Post, User } from 'shared/types/domain';
-
-export const searchByUser = async (user: string) =>
-  await getRequest<User>(`/search/users/${user}`);
-
-export const searchByAllType = async (type: string) => {
-  await getRequest<(User | Post)[]>(`/search/all/${type}`);
-};

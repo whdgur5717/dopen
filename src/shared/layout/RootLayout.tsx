b3c317bb4f56e1014a9ac908abe97f9e9a5911a8
이종혁 <whdgur57117@gmail.com>
2024-09-16 21:57:33 +0900

    root 레이아웃에서 header 제거

--- src/shared/layout/RootLayout.tsx
+++ src/shared/layout/RootLayout.tsx
@@ -1,13 +1,9 @@
 import { Flex } from '@chakra-ui/react';
-import { useRouterState } from '@tanstack/react-router';
 import { ReactNode } from 'react';
 import { DEFAULT_WIDTH } from 'shared/constants/style';
 import Footer from 'shared/ui/Footer';
-import MainHeader from 'shared/ui/MainHeader';
 
 const RootLayout = ({ children }: { children: ReactNode }) => {
-  const a = useRouterState({ select: (state) => state.location });
-  console.log(a);
   return (
     <Flex
       position="relative"
@@ -20,9 +16,8 @@ const RootLayout = ({ children }: { children: ReactNode }) => {
       overflowY="auto"
       sx={{ '&::-webkit-scrollbar': { display: 'none' } }}
     >
-      <MainHeader />
       {children}
-      <Footer pos="absolute" />
+      <Footer />
     </Flex>
   );
 };

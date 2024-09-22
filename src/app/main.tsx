e50b2113ba3080325b94fffa33bdefda4c4b36aa
이종혁 <whdgur57117@gmail.com>
2024-09-22 17:25:30 +0900

    default queryOptions 변경

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -2,13 +2,19 @@ import { ChakraProvider, ColorModeScript } from '@chakra-ui/react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { RouterProvider, createRouter } from '@tanstack/react-router';
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import { routeTree } from './routeTree.gen.ts';
 import { theme } from './theme/index.ts';
 
-const queryClient = new QueryClient({});
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 0,
+      refetchOnWindowFocus: false,
+    },
+  },
+});
 
 const router = createRouter({
   routeTree,

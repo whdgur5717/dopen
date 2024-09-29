716a4dc8ca06569e53a68fc6991f51b5d7ec7bcb
이종혁 <whdgur57117@gmail.com>
2024-09-29 19:50:13 +0900

    usePreserveCallback hook 생성 - 콜백함수 memoization

---
+++ src/shared/hook/usePreserveCallback.ts
@@ -0,0 +1,17 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { useCallback, useEffect, useRef } from 'react';
+
+/** @tossdocs-ignore */
+export function usePreservedCallback<Callback extends (...args: any[]) => any>(
+  callback: Callback,
+) {
+  const callbackRef = useRef<Callback>(callback);
+
+  useEffect(() => {
+    callbackRef.current = callback;
+  }, [callback]);
+
+  return useCallback((...args: any[]) => {
+    return callbackRef.current(...args);
+  }, []) as Callback;
+}

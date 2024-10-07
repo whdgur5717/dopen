fbca1d722031d8cc88457dd2e0dfbef19012a018
whdgur5717 <whdgur57117@gmail.com>
2024-10-07 23:26:40 +0900

    TanstackRouterDevtool 삭제

--- src/app/routes/__root.tsx
+++ src/app/routes/__root.tsx
@@ -1,20 +1,7 @@
 import { QueryClient } from '@tanstack/react-query';
 import { Outlet, createRootRouteWithContext } from '@tanstack/react-router';
-import React from 'react';
 import { ErrorBoundary } from 'react-error-boundary';
 import RootLayout from 'shared/layout/RootLayout';
-
-const TanStackRouterDevtools =
-  process.env.NODE_ENV === 'production'
-    ? () => null // Render nothing in production
-    : React.lazy(() =>
-        // Lazy load in development
-        import('@tanstack/router-devtools').then((res) => ({
-          default: res.TanStackRouterDevtools,
-          // For Embedded Mode
-          // default: res.TanStackRouterDevtoolsPanel
-        })),
-      );
 
 export const Route = createRootRouteWithContext<{
   queryClient: QueryClient;

206b424e6d4a659150ae80ac05549e072d62cd76
whdgur5717 <whdgur57117@gmail.com>
2024-10-18 00:13:15 +0900

    boardPage 준비중으로 변경

--- src/pages/BoardPage/index.tsx
+++ src/pages/BoardPage/index.tsx
@@ -1,32 +1,29 @@
-import { StackDivider, VStack } from '@chakra-ui/react';
-import { useSuspenseQuery } from '@tanstack/react-query';
-import { useLoaderData } from '@tanstack/react-router';
-import { postQueries } from 'entities/post/post.queries';
-import PostListItem from 'entities/post/ui/PostListItem';
+// import { StackDivider, VStack } from '@chakra-ui/react';
+// import { useSuspenseQuery } from '@tanstack/react-query';
+// import { postQueries } from 'entities/post/post.queries';
+// import PostListItem from 'entities/post/ui/PostListItem';
 
 const BoardPage = () => {
-  const channelId = useLoaderData({
-    from: '/_auth/Board/_boardlayout/$boardName/',
-  });
+  return <div>준비중입니다</div>;
+  // const { data } = useSuspenseQuery({
+  //   ...postQueries.postList({}),
+  // });
 
-  const { data } = useSuspenseQuery({
-    ...postQueries.postList({ channelId }),
-  });
-  return (
-    <VStack w="100%" spacing={0} divider={<StackDivider />}>
-      {data.map((post) => {
-        return (
-          <PostListItem
-            key={post.title.content + post.createdAt}
-            title={post.title.title}
-            author={post.author._fullName.name}
-            timeAgo={post.createdAt}
-            likeCount={post.likes?.length || 0}
-            commentCount={post.comments?.length || 0}
-          />
-        );
-      })}
-    </VStack>
-  );
+  // return (
+  //   <VStack w="100%" spacing={0} divider={<StackDivider />}>
+  //     {data.map((post) => {
+  //       return (
+  //         <PostListItem
+  //           key={post.title.content + post.createdAt}
+  //           title={post.title.title}
+  //           author={post.author._fullName.name}
+  //           timeAgo={post.createdAt}
+  //           likeCount={post.likes?.length || 0}
+  //           commentCount={post.comments?.length || 0}
+  //         />
+  //       );
+  //     })}
+  //   </VStack>
+  // );
 };
 export default BoardPage;

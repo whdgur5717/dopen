7b0c55d72da06d8d83a91335bb8057577772da3a
whdgur5717 <whdgur57117@gmail.com>
2024-10-17 23:09:47 +0900

    timerStampClient apiClient에 적용 - Api -> ApiClient로 이름 변경

--- src/shared/supabase/index.ts
+++ src/shared/supabase/index.ts
@@ -1,6 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 import { Channel } from 'entities/channel/model';
 import { Post } from 'entities/post/model';
+import { TimerStampClient } from 'features/timer/ui/api';
 
 import type { Database } from './database.types';
 
@@ -9,15 +10,17 @@ const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 const supabaseClient = createClient<Database>(supabaseUrl, supabaseKey);
 export type SupabaseClientType = typeof supabaseClient;
 
-export class API {
+class ApiClient {
   #client;
   channel;
   post;
+  timerStamp;
   constructor(client: SupabaseClientType) {
     this.#client = client;
     this.channel = new Channel(this.#client);
     this.post = new Post(this.#client);
+    this.timerStamp = new TimerStampClient(this.#client);
   }
 }
-export const client = new API(supabaseClient);
+export const client = new ApiClient(supabaseClient);
 export default supabaseClient;

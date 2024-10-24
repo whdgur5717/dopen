19524e77251163079bc84a448e021b720586019d
whdgur5717 <whdgur57117@gmail.com>
2024-10-24 19:19:47 +0900

    feat: park-ui preset 등록

--- panda.config.ts
+++ panda.config.ts
@@ -1,19 +1,26 @@
-import { preset } from '@jongh/panda-preset';
 import { defineConfig } from '@pandacss/dev';
 
 export default defineConfig({
   preflight: true,
-  presets: ['@pandacss/preset-base', preset],
+  presets: ['@pandacss/preset-base', '@park-ui/panda-preset'],
   include: ['./src/**/*.{js,jsx,ts,tsx}'],
   exclude: [],
   globalCss: {
+    html: {
+      fontSize: '62.5%',
+    },
     body: {
-      fontFamily: 'LINESeedKR-Bd',
+      fontFamily: 'pretendard',
     },
   },
   globalFontface: {
     'LINESeedKR-Bd': {
       src: 'url(https://fastly.jsdelivr.net/gh/projectnoonnu/noonfonts_11-01@1.0/LINESeedKR-Bd.woff2) format(woff2)',
+      fontWeight: 400,
+      fontStyle: 'normal',
+    },
+    pretendard: {
+      src: "url(https://fastly.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-Regular.woff) format('woff')",
       fontWeight: 400,
       fontStyle: 'normal',
     },

635a52bbbd86268c685eed41a801aef2528658b1
whdgur5717 <whdgur57117@gmail.com>
2024-10-30 04:51:58 +0900

    feat: add channel model and queries - add api related to channel

--- src/entities/channel/model/index.ts
+++ src/entities/channel/model/index.ts
@@ -13,7 +13,8 @@ export class Channel {
     const { data } = await this.#channel.select(
       `
         id,
-        name,
+        board_name,
+        display,
         posts (
           id,
           title,
@@ -23,5 +24,36 @@ export class Channel {
       `,
     );
     return data;
+  }
+
+  async getChannelId(boardName: string) {
+    const { data } = await this.#channel
+      .select('id')
+      .eq('board_name', boardName);
+    return data?.[0];
+  }
+  async getPostList(boardId: string) {
+    const { data, error } = await this.#channel
+      .select(
+        `
+      posts (
+        id,
+        title,
+        content,
+        created_at,
+        profiles (
+          id,
+          user_name,
+          avatar_url
+        )
+      )
+    `,
+      )
+      .eq('id', boardId);
+
+    if (error) throw new Error(error.message);
+    if (data === null) throw new Error('post not found');
+
+    return data[0].posts;
   }
 }

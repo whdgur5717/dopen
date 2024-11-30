b62ae004da471af41b2059ff9040abd131c1d4e6
whdgur5717 <whdgur57117@gmail.com>
2024-11-30 21:45:57 +0900

    refactor: remove client class

--- src/shared/supabase/index.ts
+++ src/shared/supabase/index.ts
@@ -1,8 +1,4 @@
 import { createClient } from '@supabase/supabase-js';
-import { Profile } from 'entities/auth/model';
-import { Channel } from 'entities/channel/model';
-import { Post } from 'entities/post/model';
-import { TimerStamp } from 'entities/timer/model';
 
 import type { Database } from './database.types';
 
@@ -11,19 +7,4 @@ const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 const supabaseClient = createClient<Database>(supabaseUrl, supabaseKey);
 export type SupabaseClientType = typeof supabaseClient;
 
-class ApiClient {
-  #client;
-  channel;
-  post;
-  timerStamp;
-  profile;
-  constructor(client: SupabaseClientType) {
-    this.#client = client;
-    this.channel = new Channel(this.#client);
-    this.post = new Post(this.#client);
-    this.timerStamp = new TimerStamp(this.#client);
-    this.profile = new Profile(this.#client);
-  }
-}
-export const client = new ApiClient(supabaseClient);
 export default supabaseClient;

c705bb70b715441bd34e6720812eeb48edfba328
whdgur5717 <whdgur57117@gmail.com>
2024-11-30 19:36:14 +0900

    feat: remove unused components and packages

--- src/app/routes/(auth)/Signup/index.lazy.tsx
+++
@@ -1,6 +0,0 @@
-import { createLazyFileRoute } from '@tanstack/react-router';
-import SignUp from 'pages/SignUpPage';
-
-export const Route = createLazyFileRoute('/(auth)/Signup/')({
-  component: () => <SignUp />,
-});
